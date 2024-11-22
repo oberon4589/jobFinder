a7c86deceae639cdf74c7be7638ede58f7eeedb5
SKINKA <oberon4589@gmail.com>
2024-11-22 08:55:34 -0300

    feat: criando o model

---
+++ models/Job.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const database = require("../database/connection");
+
+//Cria o model
+const Job = db.define("job", {
+  title: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  salary: {
+    type: Sequelize.STRING,
+  },
+  company: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  hiring: {
+    type: Sequelize.INTEGER,
+  },
+});
+
+module.exports = Job
